515c497a1dba74042c491f056f842f5e92116a41
Ismai21k <ismailkabir170@gmail.com>
2025-06-27 15:24:58 +0100

    add vercel domain to cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://movie-recommendation-app-fkow.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://movie-recommendation-app-fkow.vercel.app'],
     credentials: true,
 }));
 app.use(express.json());
